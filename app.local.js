c84ac779ac1b3aeb87501f9e510ce9a7cb9044cd
Juan Felipe <juan9505@gmail.com>
2018-10-19 02:56:25 -0500

    Add Rekognition Controller. Add POST Method for tag image.

--- app.local.js
+++ app.local.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 const port = 8080;
 
-app.listen(port);
+app.listen(port, '0.0.0.0');
 console.log(`listening on http://localhost:${port}`);
---
+++ controllers/rekognitionController.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const httpStatus = require('http-status');
+const boom = require('boom');
+const Joi = require('joi');
+const AWS = require('aws-sdk');
+const atob = require('atob');
+const asyncMiddleware = require('../middleware/async');
+
+const router = express.Router();
+
+const getBinary = base64Image => {
+  const binaryString = atob(base64Image);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i += 1) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+};
+
+router.post(
+  '/',
+  asyncMiddleware(async (req, res, next) => {
+    const schema = Joi.object().keys({
+      image: Joi.binary().encoding('base64'),
+    });
+    const { error } = Joi.validate(req.body, schema);
+    if (error) return next(boom.badRequest(error.details[0].message));
+    const { image } = req.body;
+    const params = {
+      Image: {
+        Bytes: getBinary(image),
+      },
+    };
+    const rekognition = new AWS.Rekognition({ region: 'us-west-2' });
+    try {
+      const labels = await rekognition.detectLabels(params).promise();
+      return res.status(httpStatus.OK).json(labels);
+    } catch (err) {
+      return next(boom.badImplementation(err));
+    }
+  }),
+);
+
+router.get('/', (req, res) => {
+  res.status(httpStatus.OK).json({
+    message: 'Rekognition',
+  });
+});
+
+module.exports = router;
